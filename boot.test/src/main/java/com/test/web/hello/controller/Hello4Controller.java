f4a3dd0ef33ee60fb8522950ef124b53af441528
doran1534 <doran1534@naver.com>
2021-04-19 14:18:18 +0900

    커밋만

---
+++ boot.test/src/main/java/com/test/web/hello/controller/Hello4Controller.java
@@ -0,0 +1,17 @@
+package com.test.web.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/hello4")
+public class Hello4Controller {
+	
+	@GetMapping("/main")
+	public String main() {
+		System.out.println("jenkins 테스트입니다.");
+		return "hello/main";
+	}
+
+}
